82fb4efa27cd874147a2262a787e6be408805d44
KennedyMungai <kennedymungaifmab@gmail.com>
2022-06-03 12:46:23 +0300

    Initial migrations

---
+++ IdentityDbContextCustomFields/Data/Migrations/20220603094253_InitialMigrationsWithCustomFields.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityDbContextCustomFields.Data.Migrations
+{
+    public partial class InitialMigrationsWithCustomFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
