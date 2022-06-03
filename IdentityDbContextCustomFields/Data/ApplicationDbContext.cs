045eae37888831244bba66131b5f4241a636d11e
KennedyMungai <kennedymungaifmab@gmail.com>
2022-06-03 12:52:11 +0300

    Added a table for custom user data

--- IdentityDbContextCustomFields/Data/ApplicationDbContext.cs
+++ IdentityDbContextCustomFields/Data/ApplicationDbContext.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
+using IdentityDbContextCustomFields.Data.Models;
+
 namespace IdentityDbContextCustomFields.Data;
 
 public class ApplicationDbContext : IdentityDbContext
 {
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
